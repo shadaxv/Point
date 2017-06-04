77b8c8a5453f62748df8f4a7e7c20784882b11fe
Aleksander Niedźwiedź <aleksandern1116@gmail.com>
2017-06-04 11:07:09 +0200

    Dodaj pliki projektów.

---
+++ Point/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    class Point
+    {
+        public int _coordinateX { get; set; }
+        private int _coordinateY { get; set; }
+
+        public Point(int coordinateX, int coordinateY)
+        {
+            _coordinateX = coordinateX;
+            _coordinateY = coordinateY;
+        }
+
+        public void Move(int moveCoordinateX, int moveCoordinateY)
+        {
+            _coordinateX = _coordinateX + moveCoordinateX;
+            _coordinateY = _coordinateY + moveCoordinateY;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("{0}Coordinates:{0}X coordinate: {1}{0}Y coordinate: {2}", Environment.NewLine, _coordinateX, _coordinateY);
+        }
+
+    }
+}
---
+++ Point/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Implementing all required numbers
+            /// </summary>
+            Console.Write("Specify the X coordinate: ");
+            string enteredNumberX = Console.ReadLine();
+            if (!Int32.TryParse(enteredNumberX, out int coordinateX))
+            {
+                Console.WriteLine("{0}The wrong number was given{0}", Environment.NewLine);
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.Write("Specify the Y coordinate: ");
+            string enteredNumberY = Console.ReadLine();
+            if (!Int32.TryParse(enteredNumberY, out int coordinateY))
+            {
+                Console.WriteLine("{0}The wrong number was given{0}", Environment.NewLine);
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadKey();
+                return;
+            }
+
+            /// <summary>
+            /// New object of Point class
+            /// </summary>
+            Point coord = new Point(coordinateX, coordinateY);
+
+            Console.Write("What value do you want to move point X? ");
+            string enteredNumberMoveX = Console.ReadLine();
+            if (!Int32.TryParse(enteredNumberMoveX, out int moveCoordinateX))
+            {
+                Console.WriteLine("{0}The wrong number was given{0}", Environment.NewLine);
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.Write("What value do you want to move point Y? ");
+            string enteredNumberMoveY = Console.ReadLine();
+            if (!Int32.TryParse(enteredNumberMoveY, out int moveCoordinateY))
+            {
+                Console.WriteLine("{0}The wrong number was given{0}", Environment.NewLine);
+                Console.WriteLine("Press any key to continue . . .");
+                Console.ReadKey();
+                return; 
+            }
+
+            /// <summary>
+            /// Moving the coordinates
+            /// </summary>
+            coord.Move(moveCoordinateX, moveCoordinateY);
+
+            /// <summary>
+            /// Showing the coordinates
+            /// </summary>
+            coord.Show();
+        }
+    }
+}
